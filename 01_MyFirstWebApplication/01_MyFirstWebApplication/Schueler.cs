b51dbbb622e6a8f643484076b80879aa509e9192
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2025-03-14 11:34:57 +0100

    Test Documents Copied into this Workspace | DONE

---
+++ 01_MyFirstWebApplication/01_MyFirstWebApplication/Schueler.cs
@@ -0,0 +1,55 @@
+﻿namespace _01_MyFirstWebApplication
+{
+    public class Schueler : Person
+    {
+        public string Klasse { get; set; }
+        public List<string> klassen = new List<string>();
+        DateTime Geburtstag;
+
+        public void AddKlasse(string klasse)
+        {
+            if (!klassen.Contains(klasse))
+            {
+                klassen.Add(klasse);
+            }
+        }
+        public int Alter
+        {
+            get
+            {
+                int alter = DateTime.Today.Year - Geburtstag.Year;
+                return alter;
+            }
+            set { }
+        }
+        public void ZähleSchülerProKlasse(List<Schueler> schuelerListe)
+        {
+            foreach (Schueler schueler in schuelerListe)
+            {
+                if (!klassen.Contains(schueler.Klasse))
+                {
+                    klassen.Add(schueler.Klasse);
+                }
+            }
+            foreach (string klasse in klassen)
+            {
+                int anzahl = 0;
+                foreach (Schueler schueler in schuelerListe)
+                {
+                    if (schueler.Klasse == klasse)
+                    {
+                        anzahl++;
+                    }
+                }
+                Console.WriteLine($"Klasse {klasse}: {anzahl} Schüler");
+            }
+        }
+        public Schueler(string klasse, DateTime geburttasg, string geschlecht) : base(geburttasg, geschlecht)
+
+        {
+            Geburtstag = geburttasg;
+            Klasse = klasse;
+            AddKlasse(klasse);
+        }
+    }
+}
