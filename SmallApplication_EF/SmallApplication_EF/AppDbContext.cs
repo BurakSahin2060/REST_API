846be5224d9f5412d9389c341bdad09b2b9f56d5
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2025-03-28 12:11:46 +0100

    First Commit

---
+++ SmallApplication_EF/SmallApplication_EF/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmallApplication_EF
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=people.db");
+        }
+    }
+}
---
+++ SmallApplication_EF/SmallApplication_EF/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SmallApplication_EF
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
