6cec533a58b5d9669e7d88bda239bee69ee55c87
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2025-04-24 15:27:57 +0200

    Assignment Done

--- SmallApplication_EF/SmallApplication_EF/Person.cs
+++ SmallApplication_EF/SmallApplication_EF/Person.cs
@@ -6,11 +6,8 @@ namespace SmallApplication_EF
     public class Person
     {
         public int Id { get; set; }
-
         public string Name { get; set; } = null!;
-
         public string PLZ { get; set; } = null!;
-
         public City City { get; set; } = null!;
     }
 }
