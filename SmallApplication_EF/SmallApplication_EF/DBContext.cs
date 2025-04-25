afb1a3fe5fff5d5ad5eac85244e7678d34c56b68
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2025-04-25 14:48:31 +0200

    Logger Added

--- SmallApplication_EF/SmallApplication_EF/DBContext.cs
+++ SmallApplication_EF/SmallApplication_EF/DBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SmallApplication_EF;
 
 namespace SmallApplication_EF
@@ -8,9 +9,20 @@ namespace SmallApplication_EF
         public DbSet<Person> Persons { get; set; }
         public DbSet<City> Cities { get; set; }
 
+        private static readonly ILoggerFactory _loggerFactory = 
+        LoggerFactory.Create(builder =>
+    {
+        builder
+            .AddConsole()
+            .AddFilter((category, level) =>
+                category == DbLoggerCategory.Database.Command.Name &&
+                level == LogLevel.Information);
+    });
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=mydatabase.db");
+            optionsBuilder.UseSqlite("Data Source=mydatabase.db")
+            .UseLoggerFactory(_loggerFactory);
         }
     }
 
